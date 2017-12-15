40f55b1f87aefdec7ff80521bb046c63041e9cf7
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2017-12-15 21:35:04 +0100

    fix: cleared up the routes

--- src/Root.js
+++ src/Root.js
@@ -1,25 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-/////---- routes----////
 import App from "./App.js";
 import MainPage from "./mainpage/mainpage.js";
-import SoilInfo from "./rainfall/rainfall.js";
 import UserInput from "./user-input/user-input.js";
-import Model from "./model/model.js";
 import SimpleResults from "./simple-results/simple-results.js";
-import Optimize from "./optimization/optimize";
+
 const Routing = props => (
   <BrowserRouter>
     <div>
       <Switch>
-        <Route exact path="/" component={App} />
         <Route path="/home" component={MainPage} />
-        <Route path="/soil-info" component={SoilInfo} />
         <Route path="/user-input" component={UserInput} />
-        <Route path="/model" component={Model} />
         <Route path="/simple-results" component={SimpleResults} />
-        <Route path="/optimization" component={Optimize} />
       </Switch>
     </div>
   </BrowserRouter>
