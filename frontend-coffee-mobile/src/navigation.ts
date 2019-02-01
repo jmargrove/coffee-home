13a7b808b2dddcf418235208604521c362b6d56e
t03jam8 <grovejam@gmail.com>
2019-02-01 08:36:26 +0100

    nav

--- frontend-coffee-mobile/src/navigation.ts
+++ frontend-coffee-mobile/src/navigation.ts
@@ -1,9 +1,8 @@
-import { createSwitchNavigator } from "react-navigation"
+import { createSwitchNavigator, createAppContainer } from "react-navigation"
 import { PoweredWelcomeScreen } from "./WelcomeScreen"
 import { PoweredMapScreen } from "./MapScreen"
 import { PoweredYieldScreen } from "./YeildScreen"
 import { WELCOME_SCREEN, MAP_SCREEN, YEILD_SCREEN } from "./utils/constants"
-import { createAppContainer } from "react-navigation"
 
 export const AppNavigation = createSwitchNavigator(
   {
