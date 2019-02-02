db6087632ab90b369b807f6dcfe13cdc3bdea491
t03jam8 <grovejam@gmail.com>
2019-02-03 00:12:20 +0100

    Welcome screen added

--- frontend-coffee-mobile/src/WelcomeScreen.tsx
+++ frontend-coffee-mobile/src/WelcomeScreen.tsx
@@ -1,27 +1,20 @@
 import React, { FunctionComponent } from "react"
-import {
-  Container,
-  Header,
-  Title,
-  Body,
-  Button,
-  Footer,
-  Text
-} from "native-base"
+import { Container, Button, Text, Image, H2 } from "native-base"
 import { withNavigation } from "react-navigation"
 import { NavigationProps } from "./types"
 import { SystemFlex } from "./system-components/SystemFlex"
 import { SystemContent } from "./system-components/SystemContent"
-import { SMALL, LARGE, BIG } from "./utils/theme"
+import { BIG } from "./utils/theme"
 import { SystemSpace } from "./system-components/SystemSpace"
 import { MAP_SCREEN } from "./utils/constants"
+import { View, ImageBackground } from "react-native"
 
 const ButtonLarge: FunctionComponent<any> = ({ to, navigation }) => {
   const navigationHandler = () => navigation.navigate(to)
 
   return (
-    <Button bordered rounded block large onPress={navigationHandler}>
-      <Text>Getting Started</Text>
+    <Button block large onPress={navigationHandler} dark>
+      <Text>Get Started</Text>
     </Button>
   )
 }
@@ -31,19 +24,34 @@ const PoweredButtonLarge = withNavigation(ButtonLarge)
 export const WelcomeScreen: FunctionComponent<NavigationProps> = () => {
   return (
     <Container>
-      <Header>
-        <Body>
-          <Title>Welcome</Title>
-        </Body>
-      </Header>
-      <SystemContent fill={true}>
-        <SystemFlex justify="flex-end">
-          <SystemFlex noFlex row justify="center">
-            <PoweredButtonLarge to={MAP_SCREEN} />
+      <ImageBackground
+        style={{ width: "100%", height: "100%" }}
+        source={require("./assests/coffee-farm.png")}
+      >
+        <SystemContent fill={true}>
+          <SystemFlex justify="flex-end">
+            <SystemFlex justify="center" align="center">
+              <View
+                style={{
+                  height: 200,
+                  width: 200,
+                  borderWidth: 4,
+                  borderRadius: 4,
+                  backgroundColor: "rgba(225,225,225, 0.75)"
+                }}
+              >
+                <SystemFlex justify="center" align="center">
+                  <H2>Coffee Engine</H2>
+                </SystemFlex>
+              </View>
+            </SystemFlex>
+            <SystemFlex noFlex row justify="center">
+              <PoweredButtonLarge to={MAP_SCREEN} />
+            </SystemFlex>
+            <SystemSpace size={BIG} />
           </SystemFlex>
-          <SystemSpace size={BIG} />
-        </SystemFlex>
-      </SystemContent>
+        </SystemContent>
+      </ImageBackground>
     </Container>
   )
 }
--- frontend-coffee-mobile/src/system-components/SystemContent.tsx
+++ frontend-coffee-mobile/src/system-components/SystemContent.tsx
@@ -4,5 +4,5 @@ import { Content } from "native-base"
 export const SystemContent = styled(Content).attrs(({ fill }: any) => ({
   contentContainerStyle: fill ? { flex: 1 } : {}
 }))<any>`
-  background-color: ${({ theme }) => "white"};
+  background-color: rgba(255, 255, 255, 0.5);
 `
