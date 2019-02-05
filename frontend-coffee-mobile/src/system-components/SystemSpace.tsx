f465eba5b9cf44fcf9be2558d83839b70a66eea0
t03jam8 <grovejam@gmail.com>
2019-02-05 22:42:37 +0100

    System Space

--- frontend-coffee-mobile/src/system-components/SystemSpace.tsx
+++ frontend-coffee-mobile/src/system-components/SystemSpace.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components"
 import { View } from "react-native"
+import { System } from "./types"
 
-export const SystemSpace = styled(View)<any>`
-max-width: ${({ theme, size }) => size && theme.sizes[size]}
-min-width: ${({ theme, size }) => size && theme.sizes[size]}
-max-height: ${({ theme, size }) => size && theme.sizes[size]}
-min-height: ${({ theme, size }) => size && theme.sizes[size]}
+export const SystemSpace = styled(View)<System.SpaceProps>`
+  max-width: ${({ theme, size }) => size && theme.sizes[size]};
+  min-width: ${({ theme, size }) => size && theme.sizes[size]};
+  max-height: ${({ theme, size }) => size && theme.sizes[size]};
+  min-height: ${({ theme, size }) => size && theme.sizes[size]};
 `
--- frontend-coffee-mobile/src/system-components/types.d.ts
+++ frontend-coffee-mobile/src/system-components/types.d.ts
@@ -13,8 +13,32 @@ export declare namespace System {
     children: ReactNode
     fill?: boolean
   }
-  interface Flex {}
-  interface Space {}
+
+  interface FlexProps {
+    noFlex?: boolean
+    justify?:
+      | "start"
+      | "center"
+      | "space-between"
+      | "space-around"
+      | "space-evenly"
+      | "flex-start"
+      | "flex-end"
+    alignContent?: "start" | "center" | "space-between" | "space-around"
+    align?: "stretch" | "center" | "start" | "end" | "flex-start" | "flex-end"
+    row?: boolean
+  }
+
+  interface SpaceProps {
+    size:
+      | "ATOMIC"
+      | "SMALL"
+      | "REGULAR"
+      | "MEDIUM"
+      | "BIG"
+      | "LARGE"
+      | "MASSIVE"
+  }
   interface Text {}
   interface Title {}
 }
